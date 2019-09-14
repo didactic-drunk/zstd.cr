f571ba6c8adde1ef4eda96b37e489c0cab8927d1
Didactic Drunk <1479616+didactic-drunk@users.noreply.github.com>
2019-09-14 06:16:41 -0700

    Travis cache dir.

--- build/travis_clone.sh
+++ build/travis_clone.sh
@@ -2,10 +2,21 @@
 
 set -e
 
-if [ ! -f zstd.git ]; then
-	git clone --branch=master https://github.com/facebook/zstd.git
+cd $HOME
+
+if [ ! -d zstd ]; then
+	git clone --single-branch --branch=master https://github.com/facebook/zstd.git
 fi
 
 cd zstd/lib
-git pull
-make clean && make && sudo make install
+
+git fetch
+
+if [ `git rev-list HEAD...origin/master --count` != 0 ]; then
+	# Only run when changed.
+	make clean && make
+else
+	echo "zstd up to date"
+fi
+
+sudo make install
